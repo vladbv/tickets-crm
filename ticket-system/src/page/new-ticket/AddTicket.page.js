b6886335f36d69cbf0292ec4b5ca9ec2afd3d699
vladbv <vladimirbhelp@gmail.com>
2022-05-07 19:18:26 +0300

    Ticket form is edited

--- ticket-system/src/page/new-ticket/AddTicket.page.js
+++ ticket-system/src/page/new-ticket/AddTicket.page.js
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 import {PageBreadcrumb} from '../../components/breadcrumb/Breadcrumb.comp'
 import {AddTicketForm} from '../../components/add-ticket-form/AddTicketForm.comp'
 
+import {shortText} from '../../utils/validation';
+
+const initialFrmDt = {
+ subject: '',
+ issueDate: '',
+    detail: '',
+
+
+};
+
+const initialFrmError = {
+ subject: false,
+ issueDate: false,
+    detail: false,
+
+};
+
+
+
 export const AddTicket = () => {
-	
-	const handleOnChange = (e) => {
-const {name, value} = e.target
+	const [frmData, setFrmData] = useState(initialFrmDt)
+	const [frmDataError, setFrmDataError] = useState(initialFrmError)
+useEffect(() => {}, [frmData, frmDataError]);	
+
+const handleOnChange = (e) => {
+
+const {name, value} = e.target;
+
+
+
+setFrmData({
+...frmData,
+   [name]: value,
+
+
+});
+
 		console.log(name, value);
+
 	};
+
+const handleOnSubmit = async (e) => {
+e.preventDefault();
+
+setFrmDataError(initialFrmError); 
+const isSubjectValid = await shortText(frmData.subject)
+
+    setFrmDataError({
+   ...initialFrmError,  
+      subject: !isSubjectValid,
+    }); 
+    
+console.log('Form submit request is received', frmData);
+
+};
 
 return ( 
 <div>
@@ -21,7 +70,7 @@ return (
 	</Row>
 <Row>
 <Col>
-<AddTicketForm handleOnChange={handleOnChange} />
+<AddTicketForm handleOnChange={handleOnChange} handleOnSubmit={handleOnSubmit} frmDataError = {frmDataError}  frmDt = {frmData} />
 	</Col>
 	</Row>
 
