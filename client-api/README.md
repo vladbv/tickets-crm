3d9b309c3fa4951b010a0ebc7d4294956b7a7d9f
vladbv <vladimirbhelp@gmail.com>
2022-07-24 19:00:43 +0300

    Work on API and routing

---
+++ client-api/README.md
@@ -0,0 +1,17 @@
+# Ticket CRM API
+
+This API is part of the creation of the Tickets CRM with the MERN stack
+
+## How to use
+
+- run `git clone ...`   
+- run `npm start`
+
+Note: Make sure to have nodemon installed in your system, otherwise you can install it as a dev dependencies in the project
+
+## API resources
+
+### User API resources
+
+All the user API router follows `/v1/user/`
+
--- client-api/app.js
+++ client-api/app.js
@@ -2,11 +2,28 @@ console.log('got hit');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+// API security
+app.use(helmet());
+
+// handle CORS error
+app.use(cors())
+
+// Logger
+app.use(morgan('tiny'));
+
+// Setting body bodyParser
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001
 
 app.use('/', (req, res) => {
-res.json({message: 'Test'});
+res.json({message: 'Test!'});
 })
 
 app.listen(port, () => {
