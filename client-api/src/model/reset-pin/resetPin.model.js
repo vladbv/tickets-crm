f488a2b850f1ac197930048f07473f33a550c1af
vladbv <vladimirbhelp@gmail.com>
2022-11-28 21:04:05 +0200

    Working on Password reset system

--- client-api/src/model/reset-pin/resetPin.model.js
+++ client-api/src/model/reset-pin/resetPin.model.js
@@ -1,76 +1,23 @@
 const { token } = require("morgan");
-const { UserSchema } = require("./User.schema");
+const { RestPinSchema } = require("./RestPin.schema");
 
-const insertUser = (userObj) => {
+const setPasswordRestPin = (userObj) => {
+  
+  const randPin = 6252642;
+
+  const restObj = {
+    email,
+    pin: randPin
+  }
+
   return new Promise((resolve, reject) => {
-    UserSchema(userObj)
+    RestPinSchema(restObj)
       .save()
       .then((data) => resolve(data))
       .catch((error) => reject(error));
   });
 };
-
-const getUserById = (_id) => {
-    return new Promise((resolve, reject) => {
-      if (!_id) return false;
-  
-      try {
-        UserSchema.findOne({ _id }, (error, data) => {
-          if (error) {
-            console.log(error);
-            reject(error);
-          }
-          resolve(data);
-        });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  };
-  
-  const getUserByEmail = (email) => {
-    return new Promise((resolve, reject) => {
-      if (!email) return false;
-  
-      try {
-        UserSchema.findOne({ email }, (error, data) => {
-          if (error) {
-            console.log(error);
-            reject(error);
-          }
-          resolve(data);
-        });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  };
-
-  const storeUserRefreshJWT = (_id, token) => {
-    return new Promise((resolve, reject) => {
-      try {
-        UserSchema.findOneAndUpdate(
-          { _id },
-          {
-        "refreshJWT.token": token, "refreshJWT.addedAt": Date.now()
-          },
-          { new: true }
-        )
-          .then((data) => resolve(data))
-          .catch((error) => {
-            console.log(error);
-            reject(error);
-          });
-      } catch (error) {
-        console.log(error);
-        reject(error);
-      }
-    });
-  };
   
 module.exports = {
-    insertUser,
-    getUserByEmail,
-    getUserById, 
-    storeUserRefreshJWT,
+ setPasswordRestPin,
 }
