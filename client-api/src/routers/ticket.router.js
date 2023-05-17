d58b7ef9aea63469fe8ada570dd6a4c61c2eb1cc
vladbv <vladimirbhelp@gmail.com>
2023-05-17 13:08:22 +0300

    Working on middleware authorization

--- client-api/src/routers/ticket.router.js
+++ client-api/src/routers/ticket.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {insertTicket} = require('../model/ticket/Ticket.model')
+const {userAuthorization} = require('../middleware/authorization.middleware')
+
+
 router.all('/', (req, res, next) => {
 // res.json({message: "return form ticket router"})
 
@@ -9,9 +12,11 @@ next();
 });
 
 
-router.post('/', async (req, res) => {
+router.post('/', userAuthorization, async (req, res) => {
 try{
     const {subject, sender, message} = req.body;
+
+    console.log(req.body)
 
     const ticketObj = {
         clientId: '631915f26bc7aa263cd8214c',
