dd3543f0c8b38d197a9dbba53526d95f6b459e7a
vladbv <vladimirbhelp@gmail.com>
2023-06-10 11:10:10 +0300

    Adding login reducer

--- ticket-system/src/store.js
+++ ticket-system/src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 
 import ticketsReducer from './page/ticket-list/ticketsSlice'
+import loginReducer from './components/login/loginSlice'
 
 const store = configureStore({
     reducer: {
         tickets: ticketsReducer,
+        login: loginReducer,
     },
 })
 
